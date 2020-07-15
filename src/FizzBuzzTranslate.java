48c45e70fe6dd74a0129d3f40a23eef751d3362d
admin <daominhhiep97@gmail.com>
2020-07-15 14:25:16 +0700

    [Bài tập] chương trình tính kết quả FizzBuzz

--- src/FizzBuzzTranslate.java
+++ src/FizzBuzzTranslate.java
@@ -1,20 +1,83 @@
 public class FizzBuzzTranslate {
-    public static final String MOD_3_AND_5 = "FizzBuzz";
-    public static final String MOD_3 = "Fizz";
-    public static final String MOD_5 = "Buzz";
+    public static final String FIZZ_BUZZ = "FizzBuzz";
+    public static final String FIZZ = "Fizz";
+    public static final String BUZZ = "Buzz";
 
     public static String translate(int number) {
         if (number % 3 == 0 && number % 5 == 0) {
-            return MOD_3_AND_5;
+            return FIZZ_BUZZ;
         }
-        if (number % 3 == 0) {
-            return MOD_3;
+        if (number % 3 == 0 || isContainedThree(number)) {
+            return FIZZ;
         }
-        if (number % 5 == 0) {
-            return MOD_5;
+        if (number % 5 == 0 || isContainedFive(number)) {
+            return BUZZ;
         }
+        if (number < 100) {
+            return readNumber(number);
+        }
+        return String.valueOf(number);
+    }
+
+    private static char[] getChars(int number) {
         String string_number = String.valueOf(number);
-        return string_number;
+        return string_number.toCharArray();
+    }
+
+
+    private static boolean isContainedThree(int number) {
+        char[] chars = getChars(number);
+        for (int index = 0; index < chars.length; index++) {
+            if (chars[index] == '3') {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean isContainedFive(int number) {
+        char[] chars = getChars(number);
+        for (int index = 0; index < chars.length; index++) {
+            if (chars[index] == '5') {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static String readNumber(int number) {
+        char[] chars = getChars(number);
+        String stringResult = "";
+        int index = 0, whiteSpace = index + 1;
+        for (; index < chars.length; index++, whiteSpace++) {
+            switch (chars[index]) {
+                case '1':
+                    stringResult += "mot";
+                    break;
+                case '2':
+                    stringResult += "hai";
+                    break;
+                case '4':
+                    stringResult += "bon";
+                    break;
+                case '6':
+                    stringResult += "sau";
+                    break;
+                case '7':
+                    stringResult += "bay";
+                    break;
+                case '8':
+                    stringResult += "tam";
+                    break;
+                case '9':
+                    stringResult += "chin";
+                    break;
+            }
+            if (whiteSpace < chars.length) {
+                stringResult += " ";
+            }
+        }
+        return stringResult;
     }
 }
 
--- test/FizzBuzzTranslateTest.java
+++ test/FizzBuzzTranslateTest.java
@@ -5,6 +5,11 @@ import static org.junit.jupiter.api.Assertions.*;
 class FizzBuzzTranslateTest {
 
     @Test
+    void translate2() {
+        String result = FizzBuzzTranslate.translate(2);
+        assertEquals("hai",result);
+    }
+    @Test
     void translate3() {
         String result = FizzBuzzTranslate.translate(3);
         assertEquals("Fizz",result);
@@ -20,8 +25,23 @@ class FizzBuzzTranslateTest {
         assertEquals("FizzBuzz",result);
     }
     @Test
-    void translate2() {
-        String result = FizzBuzzTranslate.translate(2);
-        assertEquals("2",result);
+    void translate26() {
+        String result = FizzBuzzTranslate.translate(26);
+        assertEquals("hai sau",result);
+    }
+    @Test
+    void translate32() {
+        String result = FizzBuzzTranslate.translate(32);
+        assertEquals("Fizz",result);
+    }
+    @Test
+    void translate56() {
+        String result = FizzBuzzTranslate.translate(56);
+        assertEquals("Buzz",result);
+    }
+    @Test
+    void translate86() {
+        String result = FizzBuzzTranslate.translate(86);
+        assertEquals("tam sau",result);
     }
 }
